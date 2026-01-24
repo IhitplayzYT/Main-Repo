e464fb4a21a4b6253ffc7715141e099b66bda9db
IhitplayzYT <ihitexe@gmail.com>
2026-01-24 20:21:57 +0530

    Added prerustc to the collection repo

--- PrerustC/target/debug/deps/PrerustC-1ce8a93b7dd80dc3.d
+++ PrerustC/target/debug/deps/PrerustC-1ce8a93b7dd80dc3.d
@@ -1,5 +1,6 @@
-/home/IhitplayzYT/C_FOLDER/PrerustC/target/debug/deps/PrerustC-1ce8a93b7dd80dc3.d: src/main.rs
+/home/IhitplayzYT/PrerustC/target/debug/deps/PrerustC-1ce8a93b7dd80dc3.d: src/main.rs src/util.rs
 
-/home/IhitplayzYT/C_FOLDER/PrerustC/target/debug/deps/libPrerustC-1ce8a93b7dd80dc3.rmeta: src/main.rs
+/home/IhitplayzYT/PrerustC/target/debug/deps/libPrerustC-1ce8a93b7dd80dc3.rmeta: src/main.rs src/util.rs
 
 src/main.rs:
+src/util.rs:
--- PrerustC/target/debug/deps/PrerustC-f7ed38de7ecc50c0.d
+++ PrerustC/target/debug/deps/PrerustC-f7ed38de7ecc50c0.d
@@ -1,5 +1,6 @@
-/home/IhitplayzYT/C_FOLDER/PrerustC/target/debug/deps/PrerustC-f7ed38de7ecc50c0.d: src/main.rs
+/home/IhitplayzYT/PrerustC/target/debug/deps/PrerustC-f7ed38de7ecc50c0.d: src/main.rs src/util.rs
 
-/home/IhitplayzYT/C_FOLDER/PrerustC/target/debug/deps/libPrerustC-f7ed38de7ecc50c0.rmeta: src/main.rs
+/home/IhitplayzYT/PrerustC/target/debug/deps/libPrerustC-f7ed38de7ecc50c0.rmeta: src/main.rs src/util.rs
 
 src/main.rs:
+src/util.rs:
