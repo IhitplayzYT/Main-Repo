3307c73b6dfb38dd62d551d6d2b566c7682785ea
IhitplayzYT <ihitexe@gmail.com>
2026-02-06 14:43:23 +0530

    UPdated PreRustC with proper .h file parsing and population of structs

---
+++ PrerustC/src/errors.rs
@@ -0,0 +1,24 @@
+#[allow(non_camel_case_types,non_snake_case,non_upper_case_globals,dead_code)]
+pub mod preprocessor {
+
+#[derive(Debug,Clone)]
+pub enum ParserError{
+Defer_err,
+Nullaccess_err,
+}
+
+impl std::fmt::Display for ParserError{
+fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+ write!(f,"{:?}:{:?} => {:?}\n",line!(),column!(),self)
+}
+}
+
+impl std::error::Error for ParserError{    
+}
+
+
+pub type ParserReturn<T> = Result<T,ParserError>;
+    
+
+
+}
